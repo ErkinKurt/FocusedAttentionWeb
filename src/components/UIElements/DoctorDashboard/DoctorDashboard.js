c10d2867170addc68bbbc61b723bd107ade2f5d5
Ersoy Efe Uruk <efeuruk1@gmail.com>
2019-04-01 23:43:11 +0300

    Doctor Dashboard is updated

--- src/components/UIElements/DoctorDashboard/DoctorDashboard.js
+++ src/components/UIElements/DoctorDashboard/DoctorDashboard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBCardHeader, MDBRow, MDBListGroup, MDBListGroupItem, MDBBadge, MDBIcon } from 'mdbreact';
-import { Bar, Pie } from 'react-chartjs-2';
+import { MDBBtn, MDBDataTable, MDBIcon  } from 'mdbreact';
 import { Redirect, withRouter } from 'react-router-dom';
 import * as ROUTES from '../../../constants/routes';
 
@@ -37,7 +36,6 @@ class DoctorDashboard extends Component {
 
 
     render() {
-
         let { isRedirectedToCreatePatient, isRedirectedToGameAdjustment } = this.state;
 
         if (isRedirectedToCreatePatient) {
@@ -52,133 +50,166 @@ class DoctorDashboard extends Component {
             )
         }
 
-        const dataBar = {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-            datasets: [
-            {
-                label: '#1',
-                data: [12, 39, 3, 50, 2, 32, 84],
-                backgroundColor: 'rgba(245, 74, 85, 0.5)',
-                borderWidth: 1
-            }, {
-                label: '#2',
-                data: [56, 24, 5, 16, 45, 24, 8],
-                backgroundColor: 'rgba(90, 173, 246, 0.5)',
-                borderWidth: 1
-            }, {
-                label: '#3',
-                data: [12, 25, 54, 3, 15, 44, 3],
-                backgroundColor: 'rgba(245, 192, 50, 0.5)',
-                borderWidth: 1
-            }
+        const data = {
+            columns: [
+              {
+                label: 'Name',
+                field: 'name',
+                sort: 'asc',
+                width: 150
+              },
+              {
+                label: 'Age',
+                field: 'age',
+                sort: 'asc',
+                width: 270
+              },
+              {
+                label: 'Gender',
+                field: 'gender',
+                sort: 'asc',
+                width: 150
+              },
+              {
+                label: 'Email',
+                field: 'email',
+                sort: 'asc',
+                width: 100
+              },
+              // If click pencil, redirect admin to update admin form, if click trash, delete user
+              {
+                label: 'Edit',
+                field: 'edit',
+                width: 10
+              },
+              {
+                label: 'Delete',
+                field: 'delete',
+                width: 10
+              }
+            ],
+        
+            // These are hard coded right now, but it will come from firebase
+            rows: [
+              {
+                name: 'Ersoy Efe Uruk',
+                age: '22',
+                gender: 'Male',
+                email: 'efeuruk@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
+              {
+                name: 'Umutcan Berk Hasret',
+                age: '22',
+                gender: 'Male',
+                email: 'umutcanberkhasret@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
+              {
+                name: 'Erkin Kurt',
+                age: '22',
+                gender: 'Male',
+                email: 'erkinkurt@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
+              {
+                name: 'Hamza Melih Bayrakdar',
+                age: '22',
+                gender: 'Male',
+                email: 'hamza@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
+              {
+                name: 'Ersoy Efe Uruk',
+                age: '22',
+                gender: 'Male',
+                email: 'efeuruk@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
+              {
+                name: 'Ersoy Efe Uruk',
+                age: '22',
+                gender: 'Male',
+                email: 'efeuruk@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
+              {
+                name: 'Ersoy Efe Uruk',
+                age: '22',
+                gender: 'Male',
+                email: 'efeuruk@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
+              {
+                name: 'Ersoy Efe Uruk',
+                age: '22',
+                gender: 'Male',
+                email: 'efeuruk@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
+              {
+                name: 'Ersoy Efe Uruk',
+                age: '22',
+                gender: 'Male',
+                email: 'efeuruk@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
+              {
+                name: 'Ersoy Efe Uruk',
+                age: '22',
+                gender: 'Male',
+                email: 'efeuruk@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
+              {
+                name: 'Ersoy Efe Uruk',
+                age: '22',
+                gender: 'Male',
+                email: 'efeuruk@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
+              {
+                name: 'Ersoy Efe Uruk',
+                age: '22',
+                gender: 'Male',
+                email: 'efeuruk@gmail.com',
+                edit: <MDBBtn size="sm" outline color="primary"><MDBIcon icon="pencil-alt" /></MDBBtn>,
+                delete: <MDBBtn size="sm" outline color="danger"><MDBIcon icon="user-times" /></MDBBtn>
+              },
             ]
-        };
-
-        const barChartOptions = {
-            responsive: true,
-            maintainAspectRatio: false,
-            scales: {
-            xAxes: [{
-                barPercentage: 1,
-                gridLines: {
-                display: true,
-                color: 'rgba(0, 0, 0, 0.1)'
-                }
-            }],
-            yAxes: [{
-                gridLines: {
-                display: true,
-                color: 'rgba(0, 0, 0, 0.1)'
-                },
-                ticks: {
-                beginAtZero: true
-                }
-            }]
-            }
-        }
-
-        const dataPie = {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-            datasets: [
-            {
-                data: [300, 50, 100, 40, 120, 24, 52],
-                backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360', '#ac64ad'],
-                hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774', '#da92db']
-            }
-            ]
-        }
+          };
+        
         return (
             <div className = "responsive" >
                 <h2>Welcome Doctor</h2>
+                <h4>Here is the list of Patients</h4>
+                <hr />
                 {/* Redirect to create patient form*/}
                 <MDBBtn color="elegant" onClick={this.redirectToCreatePatientForm}>Create Patient</MDBBtn>
-                {/* Redirect to datatable, in datatable, edit patient and show patient's results
-                Maybe it can be unnecessary we can show it at the beginning, when the page opens */}
-                <MDBBtn color="elegant">Show Patients</MDBBtn>
+                {/* Adjustment of test difficulties for current scenario */}
+                <MDBBtn color="elegant" onClick={this.redirectToGameAdjustmentForm}>Adjustments for Test</MDBBtn>
                 {/* Redirect to get report form, in which user will select either in time 
                 based or scenario based reports */}
                 <MDBBtn color="elegant">Get Report</MDBBtn>
                 {/* Send an email to himself/herself of some specific reports */}
                 <MDBBtn color="elegant">Email Report</MDBBtn> 
-                {/* Adjustment of test difficulties for current scenario */}
-                <MDBBtn color="elegant" onClick={this.redirectToGameAdjustmentForm}>Adjustments for Test</MDBBtn> 
+                {/* Redirect to datatable, in datatable, edit patient and show patient's results
+                Maybe it can be unnecessary we can show it at the beginning, when the page opens */}
+                <MDBBtn color="elegant">Show Patients</MDBBtn>
                 <hr />
-                {/* Tables */}
-                <MDBRow className="mb-4">
-                    <MDBCol md="8" className="mb-4">
-                        <MDBCard className="mb-4">
-                            <MDBCardBody>
-                                <Bar data={dataBar} height={500} options={barChartOptions} />
-                            </MDBCardBody>
-                        </MDBCard>
-                    </MDBCol>
-                    <MDBCol md="4" className="mb-4">
-                        <MDBCard className="mb-4">
-                            <MDBCardHeader>Pie chart</MDBCardHeader>
-                            <MDBCardBody>
-                                <Pie data={dataPie} height={300} options={{responsive: true}} />
-                            </MDBCardBody>
-                        </MDBCard>
-                        <MDBCard className="mb-4">
-                            <MDBCardBody>
-                                <MDBListGroup className="list-group-flush">
-                                    <MDBListGroupItem>
-                                        Sales
-                                        <MDBBadge color="success-color" pill className="float-right">
-                                            22%
-                                            <MDBIcon icon="arrow-up" className="ml-1"/>
-                                        </MDBBadge>
-                                    </MDBListGroupItem>
-                                    <MDBListGroupItem>
-                                        Traffic
-                                        <MDBBadge color="danger-color" pill className="float-right">
-                                            5%
-                                            <MDBIcon icon="arrow-down" className="ml-1"/>
-                                        </MDBBadge>
-                                    </MDBListGroupItem>
-                                    <MDBListGroupItem>
-                                        Orders
-                                        <MDBBadge color="primary-color" pill className="float-right">
-                                            14
-                                        </MDBBadge>
-                                    </MDBListGroupItem>
-                                    <MDBListGroupItem>
-                                        Issues
-                                        <MDBBadge color="primary-color" pill className="float-right">
-                                            123
-                                        </MDBBadge>
-                                    </MDBListGroupItem>
-                                    <MDBListGroupItem>
-                                        Messages
-                                        <MDBBadge color="primary-color" pill className="float-right">
-                                            8
-                                        </MDBBadge>
-                                    </MDBListGroupItem>
-                                </MDBListGroup>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </MDBCol>
-                </MDBRow>
+                {/* Datatable */}
+                <MDBDataTable striped bordered small data={data} />
             </div>
         )
     }
