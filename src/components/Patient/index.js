ac9c144fe5f3b223b96ba02464b684c3333a69f6
Ersoy Efe Uruk <efeuruk1@gmail.com>
2019-04-02 00:34:48 +0300

    Create PC Form is created

--- src/components/Patient/index.js
+++ src/components/Patient/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import PatientDashboard from '../UIElements/PatientDashboard/PatientDashboard';
+import { FirebaseContext } from '../Firebase';
 
 export default class Patient extends Component {
   render() {
     return (
-      <div>
-        <PatientDashboard />
-      </div>
+      <FirebaseContext.Consumer>
+        {firebase => <PatientDashboard firebase={firebase} />}
+      </FirebaseContext.Consumer>
     )
   }
 }
--- src/components/UIElements/CreatePCForm/CreatePCForm.js
+++ src/components/UIElements/CreatePCForm/CreatePCForm.js
@@ -4,7 +4,32 @@ import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
 class CreatePCForm extends Component {
   render() {
     return (
-      <p>Add A PC</p>
+      <MDBContainer>
+                <MDBRow className="d-flex justify-content-center mt-5">
+                    <MDBCol md="6">
+                        <form>
+                            <p className="h5 text-center mb-4">Add PC</p>
+                            <div className="grey-text">
+                                <MDBInput
+                                    label="PC Name"
+                                    value=""
+                                    name="pcName"
+                                    icon="desktop"
+                                    group
+                                    type="text"
+                                    validate
+                                    error="wrong"
+                                    success="right"
+                                    onChange={this.onChange}
+                                />   
+                            </div>
+                            <div className="text-center">
+                                <MDBBtn color="success">Add PC</MDBBtn>
+                            </div>
+                        </form>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
     )
   }
 }
